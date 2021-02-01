822d5097870f35d0505ab24acbc7446f60e7804f
Elena-GHub <cabjelen@yahoo.es>
2021-02-01 18:55:58 +0100

    adds letters only validation to meal_type field

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -1,5 +1,7 @@
 class Entry < ApplicationRecord
     validates :meal_type, :calories, :proteins, :carbohydrates, :fats, presence: true
+    validates :meal_type, format: { with: /\A[a-zA-Z]+\z/,
+        message: "only allows letters" }
 
     def day
         self.created_at.strftime("%b %e, %Y")
