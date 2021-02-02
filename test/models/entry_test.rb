de0578c386d8db8518008eb64edd15eccaf4ab1a
Elena-GHub <cabjelen@yahoo.es>
2021-02-02 15:29:58 +0100

    adds Entry model tests

--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class EntryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "is valid with all valid and compulsory attributes" do
+    entry = Entry.new(meal_type: "lunch", calories: 500, proteins: 10, carbohydrates: 100, fats: 5)
+    assert entry.save
+  end
+
+  test "should not save entry without meal_type" do
+    entry = Entry.new(calories: 400, proteins: 10, carbohydrates: 100, fats: 5)
+    assert_not entry.save
+  end
+
+  test "should not save entry without calories" do
+    entry = Entry.new(meal_type: "lunch", proteins: 10, carbohydrates: 100, fats: 5)
+    assert_not entry.save
+  end
+
+  test "should not save entry without proteins" do
+    entry = Entry.new(meal_type: "lunch", calories: 400, carbohydrates: 100, fats: 5)
+    assert_not entry.save
+  end
+  test "should not save entry without carbohydates" do
+    entry = Entry.new(meal_type: "lunch", calories: 400, proteins: 10, fats: 5)
+    assert_not entry.save
+  end
+
+  test "should not save entry without fats" do
+    entry = Entry.new(meal_type: "lunch", calories: 400, proteins: 10, carbohydrates: 100)
+    assert_not entry.save
+  end
 end
